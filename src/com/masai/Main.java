83444f6ef0f60f8c516ebd894b9c9b2ed521b68f
abhay <itsyourabhay@gmail.com>
2023-03-30 23:24:17 +0530

    Day-3

--- src/com/masai/Main.java
+++ src/com/masai/Main.java
@@ -1,5 +1,10 @@
 package com.masai;
 
 public class Main {
-
+public static void main(String[] args) {
+	System.out.println("To Login as Admin - Enter 1");
+	System.out.println("To Login as Faculty - Enter 2");
+	System.out.println("To Register as Faculty - Enter 3");
+	System.out.println("To Exit from the Site - Enter 0");
+}
 }
